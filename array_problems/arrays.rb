32b144898b2fad0cdf48f8439f3acf9cb157a38c
Ashley McKemie <a.mckemie@utexas.edu>
2014-05-04 20:09:22 -0500

    Updating all changes made to algorithms directory

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,11 +3,43 @@ require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
+
+    max = array[0]
+
+    for num in 1...array.length
+      max = array[num] if array[num] > max
+    end
+
+    max
   end
 
   def self.middle_element(array)
+    if array.length == 0
+      return nil
+    else
+      average = 0
+    end
+
+    if array.length % 2 == 0
+      average = (array[array.length/2] + array[array.length/2 - 1]) / 2.0
+    else
+      average = array[array.length/2]
+    end
+
+    average
   end
 
   def self.sum_arrays(array1, array2)
+    if array1.length == 0 && array2.length == 0
+      return []
+    end
+
+    new_array = []
+
+    for num in 0...array1.length
+      new_array << array1[num] + array2[num]
+    end
+
+    new_array
   end
 end
--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,41 @@
 module Set1
+  # O(n)
   def self.swap_small(array)
+    smallest = array[0]
+    index = 0
+    for num in 0...array.length
+      if array[num] < smallest
+        smallest = array[num]
+        index = num
+      end
+    end
+    array[0], array[index] = array[index], array[0]
+    array
   end
 
+  #0(n^2)
   def self.find_sum_2(array, sum = 0)
+    return false if array.length == 0
+
+    (0...array.length).each do |i|
+      (i...array.length).each do |j|
+        return true if array[i] + array[j] == sum
+      end
+    end
+    false
   end
 
-  def self.find_sum_3(array)
+  #O(n^3)
+  def self.find_sum_3(array, sum = 0)
+    return false if array.length == 0
+
+    for i in 0...array.length
+      for j in i...array.length
+        for k in j...array.length
+          return true if array[i] + array[j] + array[k]== sum
+        end
+      end
+    end
+    false
   end
 end
