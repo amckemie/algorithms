035641f7bb0833db4d7181238eca6fcbbab54782
Ashley McKemie <ashley.mckemie@gmail.com>
2014-05-13 13:09:50 -0500

    Add training algorithm and predict algorithm

--- project_gutenberg/lib/complex_predictor.rb
+++ project_gutenberg/lib/complex_predictor.rb
@@ -1,4 +1,6 @@
 require_relative 'predictor'
+require 'pry-debugger'
+
 
 class ComplexPredictor < Predictor
   # Public: Trains the predictor on books in our dataset. This method is called
@@ -7,6 +9,25 @@ class ComplexPredictor < Predictor
   # Returns nothing.
   def train!
     @data = {}
+
+    @all_books.each do |category, books|
+      @data[category] = {
+        words: {}
+      }
+      books.each do |filename, tokens|
+        # First 20% of words are used, last 80% ignored. Gives more weight to intro sections
+        for i in (0...tokens.length/5)
+          if good_token?(tokens[i])
+            if @data[category][:words].has_key?(tokens[i].to_sym)
+              @data[category][:words][tokens[i].to_sym] += 1
+            else
+              @data[category][:words][tokens[i].to_sym] = 1
+            end
+          end
+        end
+      end
+      data = @data[category][:words].sort_by{|key,value| value}.reverse!
+    end
   end
 
   # Public: Predicts category.
@@ -15,8 +36,30 @@ class ComplexPredictor < Predictor
   #
   # Returns a category.
   def predict(tokens)
-    # Always predict astronomy, for now.
-    :astronomy
+    tokens_hash = {}
+    for i in (0...tokens.length/5) do
+      if good_token?(tokens[i])
+        if tokens_hash.has_key?(tokens[i].to_sym)
+          tokens_hash[tokens[i].to_sym] += 1
+        else
+          tokens_hash[tokens[i].to_sym] = 1
+        end
+      end
+    end
+
+    max_word = tokens_hash.max_by{|key,value| value}.first
+    min_place = 9999999
+    category_holder = nil
+    @data.each do |category, words|
+      category_words = @data[category][:words].keys
+        for i in (0...category_words.length)
+          if category_words[i] == max_word && i < min_place
+            min_place = i
+            category_holder = category
+          end
+        end
+      # end
+    end
+    category_holder
   end
 end
-
